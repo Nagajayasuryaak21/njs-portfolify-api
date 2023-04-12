8ad49ddec4734dd425c7ede1e88016c97e59efcd
Nagajayasuryaak21 <88658242+Nagajayasuryaak21@users.noreply.github.com>
2023-04-12 13:20:32 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,15 @@ const { Data } = require("../models/data");
 //const bcrypt = require("bcrypt");
 const bcrypt = require('bcryptjs');
 router.post("/", async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   try {
     console.log(req.body);
     // const { error } = validate(req.body);
